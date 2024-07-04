3e028642a96f7ece1b28306bc1d0b43623781397
Bungle <hn.kim@ecubelabs.com>
2024-07-04 10:56:37 +0900

    fix: 영어로 응답 출력

--- dist/index.js
+++ dist/index.js
@@ -72918,7 +72918,6 @@ async function analyzeCode(parsedDiff, prDetails) {
 function createPrompt(file, chunk, prDetails) {
     return `Your task is to review pull requests. Instructions:
 - Provide the response in following JSON format:  {"reviews": [{"lineNumber":  <line_number>, "reviewComment": "<review comment>"}]}
-- Please fill "reviewComment" in Korean.
 - Do not give positive comments or compliments.
 - Provide comments and suggestions ONLY if there is something to improve, otherwise "reviews" should be an empty array.
 - Write the comment in GitHub Markdown format.
--- src/index.ts
+++ src/index.ts
@@ -88,7 +88,6 @@ async function analyzeCode(
 function createPrompt(file: File, chunk: Chunk, prDetails: PRDetails): string {
   return `Your task is to review pull requests. Instructions:
 - Provide the response in following JSON format:  {"reviews": [{"lineNumber":  <line_number>, "reviewComment": "<review comment>"}]}
-- Please fill "reviewComment" in Korean.
 - Do not give positive comments or compliments.
 - Provide comments and suggestions ONLY if there is something to improve, otherwise "reviews" should be an empty array.
 - Write the comment in GitHub Markdown format.
